a6cc514d00883c0865a110eb7e4f67d044231e9e
Strahinja <strahinjav98@gmail.com>
2023-01-30 19:14:13 +0100

    lesson 5 completed and works (lesson 4 was just an explination for auto importing components

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -33,13 +33,11 @@
 
       <h3>Extras</h3>
       <div>
-        <input type="checkbox" v-model="event.extras.catering" class="field" />
-        <label>Catering</label>
+        <BaseCheckbox v-model="event.extras.catering" label="Catering" />
       </div>
 
       <div>
-        <input type="checkbox" v-model="event.extras.music" class="field" />
-        <label>Live music</label>
+        <BaseCheckbox v-model="event.extras.music" label="Live Music" />"
       </div>
 
       <button class="button -fill-gradient" type="submit">Submit</button>
@@ -48,7 +46,6 @@
 </template>
 
 <script>
-
 export default {
   data () {
     return {
