1da53adbbb0f5f1762a8a5aa252a0caeecf01f91
Strahinja <strahinjav98@gmail.com>
2023-01-31 18:08:05 +0100

    lesson9 mostly informative but working

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -8,19 +8,23 @@
         label="Select a Category"
       />
 
-      <h3>Name & describe your event</h3>
+      <fieldset>
+        <legend>Name & describe your event</legend>
 
-      <BaseInput v-model="event.title" label="Title" type="text" />
+        <BaseInput v-model="event.title" label="Title" type="text" />
 
-      <BaseInput v-model="event.description" label="Description" type="text" />
+        <BaseInput v-model="event.description" label="Description" type="text" />
+      </fieldset>
 
-      <h3>Where is your event?</h3>
+      <fieldset>
+        <legend>Where is your event?</legend>
 
-      <p>{{ event }}</p>
+        <BaseInput v-model="event.location" label="Location" type="text" />
+      </fieldset>
 
-      <BaseInput v-model="event.location" label="Location" type="text" />
-
-      <h3>Are pets allowed?</h3>
+      <fieldset>
+        <legend>Pets</legend>
+        <p>Are pets allowed?</p>
       <div>
         <BaseRadioGroup
           v-model="event.pets"
@@ -28,8 +32,10 @@
           :options="petOptions"
         />
       </div>
+      </fieldset>
 
-      <h3>Extras</h3>
+      <fieldset>
+        <legend>Extras</legend>
       <div>
         <BaseCheckbox v-model="event.extras.catering" label="Catering" />
       </div>
@@ -37,6 +43,7 @@
       <div>
         <BaseCheckbox v-model="event.extras.music" label="Live Music" />
       </div>
+      </fieldset>
 
       <button class="button -fill-gradient" type="submit">Submit</button>
     </form>
@@ -91,3 +98,17 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  fieldset {
+    border: 0;
+    margin: 0;
+    padding: 0;
+  }
+
+  legend {
+    font-size: 28px;
+    font-weight: 700;
+    margin-top: 20px;
+  }
+</style>
